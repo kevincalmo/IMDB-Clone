579873086c4836f0fdfacf1a9cf0fa039bed2069
Kévin F. CALMO <kevinfcalmo@gmail.com>
2023-05-20 13:59:17 -0400

    eslint update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image'
-import RootLayout from './layout'
+import RootLayout from "./layout";
 
 export default function Home() {
   return (
     <RootLayout>
-      <h1 className='text-blue-700'>Home</h1>
+      <h1 className="text-blue-700">Home</h1>
     </RootLayout>
-  
-  )
+  );
 }
