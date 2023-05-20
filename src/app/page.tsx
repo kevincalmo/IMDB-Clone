05c8e2db69deb60be47c17f877e5319735b7bb44
Kévin F. CALMO <kevinfcalmo@gmail.com>
2023-05-20 13:31:38 -0400

    Install NextJS

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import RootLayout from './layout'
+
+export default function Home() {
+  return (
+    <RootLayout>
+      <h1>Home</h1>
+    </RootLayout>
+  
+  )
+}
