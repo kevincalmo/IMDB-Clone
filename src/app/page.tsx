a2eb109965cd4acb68ebfa3038f74769757e5e87
Kévin F. CALMO <kevinfcalmo@gmail.com>
2023-05-20 13:51:27 -0400

    Test tailwindcss inside this project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import RootLayout from './layout'
 export default function Home() {
   return (
     <RootLayout>
-      <h1>Home</h1>
+      <h1 className='text-blue-700'>Home</h1>
     </RootLayout>
   
   )
