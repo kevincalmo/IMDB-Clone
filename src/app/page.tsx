65a6762379b2dc7e529f2d72618b0a1f32b9208a
Kévin F. CALMO <kevinfcalmo@gmail.com>
2023-05-20 15:02:15 -0400

    add header to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
 import RootLayout from "./layout";
 
 export default function Home() {
   return (
     <RootLayout>
+      <Header />
       <h1 className="text-blue-700">Home</h1>
     </RootLayout>
   );
