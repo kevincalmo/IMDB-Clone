b30b13d6f36e2d66a26039e36ec0b41af141fe04
Kévin F. CALMO <kevinfcalmo@gmail.com>
2023-05-21 22:45:51 -0400

    change nextJs Image component to img html

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function loading() {
   return (
     <div className="flex justify-center">
-      <Image className="h-96" src="spinner.svg" alt="loading..." />
+      <img className="h-96" src="spinner.svg" alt="loading..." />
     </div>
   );
 }
